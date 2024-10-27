5fe12dfa56f579376828ddc18b97320ece12c8e4
Aditya41205 <157035985+Aditya41205@users.noreply.github.com>
2024-10-27 07:03:59 +0530

    updated

--- interactionsSC.js
+++ interactionsSC.js
@@ -111,5 +111,10 @@ const contractinteraction=async()=>{
     const contractBalance = await walletContract.contractBalance();
     const balethContract = ethers.utils.formatEther(contractBalance);
     console.log("Contract Balance:", balethContract);
+    const userBalance = await walletContract.accountBalance(
+         "0xBE4024Fa7461933F930DD3CEf5D1a01363E9f284"
+       );
+       const balethUser = ethers.utils.formatEther(userBalance);
+       console.log("User Balance:", balethUser);
 };
 contractinteraction();
